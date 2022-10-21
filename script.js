6e3b94a3c36e5d314e02235007aa1b806f100799
Wesley Guerra <wesley.guerra@acpgroup.com.br>
2022-10-20 23:07:52 -0300

    Update

--- script.js
+++ script.js
@@ -3,9 +3,39 @@ document.body.addEventListener('keyup', function(event) {
     playSound(event.code.toLocaleLowerCase())
 })
 
+document.querySelector('.composer button').addEventListener('click', function(event) {
+    let song = document.querySelector('#input').value;
+
+    if (song !== '') {
+        let songArray = song.split(''); // transforma em array 
+        playComposition(songArray)
+    }
+})
+
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+    console.log(keyElement)
     if (audioElement) {
-        audioElement.play()
+        audioElement.currentTime = 0; // reset the current time
+        audioElement.play() // play the sound
+    }
+
+    if (keyElement) {
+        keyElement.classList.add('active')
+        setTimeout(() => {
+            keyElement.classList.remove('active')
+        }, 300)
+    }
+}
+
+function playComposition(songArray) {
+    let wait = 0;
+    for (let songItem of songArray) {
+        setTimeout(() => {
+            playSound(`key${songItem}`)
+        }, wait)
+
+        wait += 250;
     }
 }
